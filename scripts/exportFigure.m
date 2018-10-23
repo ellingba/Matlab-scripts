e11b1a73032488fba3c377c2936c025b789d77d5
ellingba <42233960+ellingba@users.noreply.github.com>
2018-10-23 16:51:06 +0200

    changed exportFigure to allow for overwrite

--- scripts/exportFigure.m
+++ scripts/exportFigure.m
@@ -1,9 +1,10 @@
-function exportFigure(fig,saveFolder,name,format)
+function exportFigure(greenLight,name,fig,saveFolder,format)
 %EXPORTFIGURE prints a figure in the wanted format after checking that the
 %folder doesn't contain a file with the same name and format
+%GREENLIGHT: 1 if you want to overwrite, default is 0
+%NAME: wanted file name, default is figure's title
 %FIG: figure handle
 %SAVEFOLDER: Folder location, default is current folder
-%NAME: wanted file name, default is figure's title
 %FORMAT: extention of file to print, default is png
 %Other format include: 'pdf' and 'eps'
 
@@ -14,17 +15,22 @@ setDefaultValue('saveFolder',[pwd,'\']);
 a = gca;
 setDefaultValue('name',a.Title.String);
 setDefaultValue('format','png');
+setDefaultValue('greenLight',0)
 
+%Check if there's a dot in name
+if sum(name == '.')
+    error('Name (or Title) has a dot!')
+end
 
+%Check extention format
 possExt = ["-dpng","-dpdf","-deps"];
 printFormat= char(possExt(strcmp(['-d',format],possExt)));
 if isempty(printFormat)
     error('Format must be ''png'', ''pdf'', ''eps'' or empty')
 end
 
-
 % Check if name already exists in folder
-greenLight = 0;
+% greenLight = 0;
 nameTemp = name;
 while ~greenLight
     check = dir(fullfile(saveFolder,[nameTemp,'.',format]));
